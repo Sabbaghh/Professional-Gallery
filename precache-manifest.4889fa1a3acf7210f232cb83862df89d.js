220be95d06ed01eb9b4c84ac44bf3f0b8166bbc9
abdallah Sabbagh <afsabbagh9@gmail.com>
2020-10-26 03:26:41 +0300

    Updates

---
+++ precache-manifest.4889fa1a3acf7210f232cb83862df89d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8c6eaf9993aa770f7b6de3115f1a375",
+    "url": "/Professional-Gallery/index.html"
+  },
+  {
+    "revision": "c161583c36eff675fbf8",
+    "url": "/Professional-Gallery/static/css/main.9914df99.chunk.css"
+  },
+  {
+    "revision": "f6e3a888118b1d764bca",
+    "url": "/Professional-Gallery/static/js/2.6a8d6a3d.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/Professional-Gallery/static/js/2.6a8d6a3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c161583c36eff675fbf8",
+    "url": "/Professional-Gallery/static/js/main.dff41754.chunk.js"
+  },
+  {
+    "revision": "a796a38ff0dd591f1d08",
+    "url": "/Professional-Gallery/static/js/runtime-main.826bb4a2.js"
+  },
+  {
+    "revision": "01195ba89ff9139b46b144a9462fca64",
+    "url": "/Professional-Gallery/static/media/Jacob.01195ba8.jpg"
+  },
+  {
+    "revision": "b561889158d4066ef224332c5d4202ab",
+    "url": "/Professional-Gallery/static/media/bg.b5618891.jpg"
+  },
+  {
+    "revision": "78a60011d84406a5090a0e98e555798f",
+    "url": "/Professional-Gallery/static/media/leftside.78a60011.jpg"
+  },
+  {
+    "revision": "0ebe0937efbeabdc28afd1efeb607b40",
+    "url": "/Professional-Gallery/static/media/lens.0ebe0937.png"
+  },
+  {
+    "revision": "11d8836a95d86aaebbb68537a330c8f2",
+    "url": "/Professional-Gallery/static/media/logo.11d8836a.png"
+  },
+  {
+    "revision": "395339e06998809a012cafc24a300dce",
+    "url": "/Professional-Gallery/static/media/rightside.395339e0.png"
+  }
+]);
